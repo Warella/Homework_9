72fc063e204f7480e541d427219a1db01900bd94
Lera <bezinskayav@gmail.com>
2022-12-21 19:04:25 +0300

    Add new files

---
+++ Exercise_64/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+Console.Clear();
+int EnterNumber(string str)
+{
+    Console.WriteLine(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+int OutputNaturelNumbers(int n, int m)
+{
+    if (n == m) 
+    {
+        return n;
+    }
+    else Console.Write($"{OutputNaturelNumbers(n, m + 1)}, ");
+    return m;
+}
+int n = EnterNumber("Input N: ");
+OutputNaturelNumbers(n, 0);
---
+++ Exercise_66/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+Console.Clear();
+int EnterNumber(string str)
+{
+    Console.WriteLine(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+int OutputSum(int m, int n)
+{
+    if (m == n) 
+    {
+        return n;
+    }
+    return n + OutputSum(m, n - 1);
+}
+int m = EnterNumber("Input M: ");
+int n = EnterNumber("Input N: ");
+Console.WriteLine(OutputSum(m, n));
